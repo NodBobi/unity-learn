5431c296436678f6d2265924e66499c802ad43d7
Luke <luukas443@gmail.com>
2022-12-23 21:32:49 +0200

    Third person movementti
    Third person movementti (ihan mytyn väliaikanen)

--- Unity Harjoittelu/Assets/Collider.cs
+++ Unity Harjoittelu/Assets/Collider.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Collider : MonoBehaviour
 {
+    public float movementSpeed = 5.0f;
+    public float mouseSensitivity = 2.0f;
+
+    private float verticalRotation = 0;
+    private float verticalRotationLimit = 60;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +19,21 @@ public class Collider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float forwardMovement = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        float sideMovement = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+
+        transform.position += transform.forward * forwardMovement;
+        transform.position += transform.right * sideMovement;
+
+        // Handle mouse input
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+
+        transform.Rotate(0, mouseX * mouseSensitivity, 0);
+
+        verticalRotation -= mouseY * mouseSensitivity;
+        verticalRotation = Mathf.Clamp(verticalRotation, -verticalRotationLimit, verticalRotationLimit);
+        Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
     }
 
     private void OnCollisionEnter(Collision collision)
