b55dc7e72d783cf2804bf8d4c9f870fa3a8f1ae6
Luke <luukas443@gmail.com>
2022-12-24 15:57:49 +0200

    Hieno peli
    tein pelin

--- Unity Harjoittelu/Assets/Collider2.cs
+++ Unity Harjoittelu/Assets/Collider2.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Collider2 : MonoBehaviour
 {
-
+    int pisteet = 0;
     // Start is called before the first frame update
     void Start()
     {
-
+        Material mat = gameObject.GetComponent<Renderer>().material;
+        mat.color = Color.red;
     }
 
     // Update is called once per frame
@@ -17,8 +19,23 @@ public class Collider2 : MonoBehaviour
         
     }
 
+    public void MoveGameObject()
+    {
+        gameObject.transform.position = new Vector3(Random.Range(-2, 85), 1, Random.Range(-34, 54));
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-
+        MoveGameObject();
+        Material mat = gameObject.GetComponent<Renderer>().material;
+        if (mat.color == Color.blue)
+        {
+            mat.color = Color.red;
+        } else if(mat.color == Color.red)
+        {
+            mat.color = Color.blue;
+        }
+        pisteet += 1;
+        Debug.Log("Pisteesi tällä hetkellä: " + pisteet);
     }
 }
